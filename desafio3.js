74959b4e98f9624ec94dac33eab3aba6b70754bb
gustavoribe1ro <gustavoribeiro99@gmail.com>
2024-01-24 18:51:07 -0300

    desafio 3

--- desafio3.js
+++ desafio3.js
@@ -29,22 +29,3 @@ class heroi{
 let mensagem = new heroi("Gustavo", "47", "ninja")
 
 mensagem.escrever()
-/*
-além disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
-
-- exibir a mensagem: "o {tipo} atacou usando {ataque}")
-- aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
-- e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
-
-se mago -> no ataque exibir (usou magia)
-se guerreiro -> no ataque exibir (usou espada)
-se monge -> no ataque exibir (usou artes marciais)
-se ninja -> no ataque exibir (usou shuriken)
-
-## Saída
-
-Ao final deve se exibir uma mensagem:
-
-- "o {tipo} atacou usando {ataque}"
-  ex: mago atacou usando magia
-*/
